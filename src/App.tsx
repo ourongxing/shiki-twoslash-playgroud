152eeeaaa07d9c5d6cb43054b6252acb396dcd08
Ou <orongxing@gmail.com>
2024-10-08 11:55:38 +0800

    pref: shiki editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "virtual:uno.css"
 import "./style/app.css"
 import "@unocss/reset/tailwind.css"
-import { useEffect, useState } from "react"
-import clsx from "clsx"
+import { useEffect, useRef, useState } from "react"
+import { useEditable } from "use-editable"
 import { ThemeToggle } from "./components/theme-toggle"
 import { render } from "./shiki"
 import { Share } from "./components/share"
 
 function CodeEditor() {
   const [input, setInput] = useState(`console.log("hello world")`)
+  const editorRef = useRef(null)
+  useEditable(editorRef, setInput)
   const [output, setOutput] = useState("")
   useEffect(() => {
     const fn = async () => {
@@ -31,13 +33,11 @@ function CodeEditor() {
         <ThemeToggle />
         <Share code={input} />
       </div>
-      <div dangerouslySetInnerHTML={{ __html: output }} />
-      <textarea
-        id="input"
-        className={clsx("outline-none absolute top-0 left-0 inset-0 caret-gray w-full h-full resize-none of-hidden bg-transparent z-10 font-mono text-transparent")}
-        spellCheck={false}
-        value={input}
-        onInput={e => setInput(e.currentTarget.value)}
+      <div
+        id="shiki-wrapper"
+        ref={editorRef}
+        className="font-mono outline-none w-full h-full"
+        dangerouslySetInnerHTML={{ __html: output }}
       />
     </div>
   )
